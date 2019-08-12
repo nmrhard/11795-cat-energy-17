11dc0f6a3a4cfd06d5069c0e572ac37935a3e2f1
nmrhard <nmrhard@gmail.com>
2019-08-12 19:57:03 +0300

    Finish form page for desktop and elements state

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,11 @@
 var toggleMenu = document.querySelector('.page-header__toggle-menu');
 var mainNav = document.querySelector('.main-nav');
 
+var form = document.querySelector('.questionnaire__data');
+var inputs = form.querySelectorAll("input");
+var mailIcon = form.querySelector(".questionnaire__icon-mail");
+var phoneIcon = form.querySelector(".questionnaire__icon-phone");
+
 mainNav.classList.remove('main-nav--nojs');
 toggleMenu.classList.remove('page-header__toggle-menu--nojs');
 
@@ -18,3 +23,13 @@ toggleMenu.addEventListener('click', function(evt) {
     mainNav.classList.add('main-nav--closed');
   }
 });
+
+for (let input of inputs) {
+  input.addEventListener('invalid', (event) => {
+    input.classList.add('data-input--error');
+    mailIcon.classList.add('questionnaire__icon-mail--error');
+    phoneIcon.classList.add('questionnaire__icon-phone-error');
+  }, false);
+}
+
+
