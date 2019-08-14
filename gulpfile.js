6ccf638c8980ffe943dc1c65f9c9e58b32a28a12
nmrhard <nmrhard@gmail.com>
2019-08-14 13:14:58 +0300

    Add js minify task

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,9 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var del = require("del");
-var htmlmin = require('gulp-htmlmin');
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
+var pipeline = require('readable-stream').pipeline;
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -59,11 +61,26 @@ gulp.task("htmlminify", function() {
   .pipe(gulp.dest("build"))
 });
 
+gulp.task("jsclean", function() {
+  return del("source/**/*.min.js");
+});
+
+gulp.task("jsminify", function () {
+  return pipeline(
+        gulp.src("source/js/**/*.js"),
+        uglify(),
+        rename({extname: ".min.js"}),
+        gulp.dest("source/js")
+  );
+});
+
+gulp.task("jscompress", gulp.series("jsclean", "jsminify"));
+
 gulp.task("copy", function(){
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
+    "source/js/**/*.min.js",
     "source/css/**",
     "source/*.ico"
   ], {
@@ -86,5 +103,5 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "htmlminify"));
+gulp.task("build", gulp.series("clean", "jscompress", "copy", "css", "htmlminify"));
 gulp.task("start", gulp.series("build", "server"));
